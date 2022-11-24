d690af6f42c19ccda8aba84f423afd6d878820c8
Lucián Blažek <lucian@ftmo.com>
2022-11-24 01:02:51 +0100

    feat(main-menu): update view

--- Assets/SpaceRogue/UI/Controller/MainMenuController.cs
+++ Assets/SpaceRogue/UI/Controller/MainMenuController.cs
@@ -6,6 +6,8 @@ namespace SpaceRogue.UI.Controller
 {
     public class MainMenuController : MonoBehaviour
     {
+        private const string ExitButtonClassName = "-btn-target-exit";
+
         public UIDocument document;
         public string initialPageState = "main";
 
@@ -16,6 +18,15 @@ namespace SpaceRogue.UI.Controller
             VisualElement root = this.document.rootVisualElement;
 
             this.menuHandler = new MenuHandler(root, this.initialPageState);
+        }
+
+        private void RegisterExitButton(VisualElement root)
+        {
+            root.Q(className: ExitButtonClassName).RegisterCallback<ClickEvent>(clickEvent =>
+            {
+                // if (clickEvent.currentTarget is not ButtonElement button) throw new NullReferenceException("Could not find button in event");
+                Application.Quit();
+            });
         }
     }
 }
